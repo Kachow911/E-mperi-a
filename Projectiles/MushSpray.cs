63272e8fbd55cbaa570ae56de0426a5b3ee625f5
Kachow911 <torniky@gmail.com>
2017-03-02 07:48:04 -0800

    good spray
    sssss

--- Projectiles/MushSpray.cs
+++ Projectiles/MushSpray.cs
@@ -34,10 +34,10 @@ namespace Emperia.Projectiles       //We need this to basically indicate the fol
            
                 if (Main.rand.Next(3) == 0)     //this defines how many dust to spawn
                 {
-                    int dust = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 17, projectile.velocity.X * 1.2f, projectile.velocity.Y * 1.2f, 75, default(Color), 3.75f);   //this defines the flames dust and color, change DustID to wat dust you want from Terraria, or add mod.DustType("CustomDustName") for your custom dust
+                    int dust = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 17, projectile.velocity.X * 1.2f, projectile.velocity.Y * 1.2f, 75, default(Color), 1.5f);   //this defines the flames dust and color, change DustID to wat dust you want from Terraria, or add mod.DustType("CustomDustName") for your custom dust
                     Main.dust[dust].noGravity = true; //this make so the dust has no gravity
                     Main.dust[dust].velocity *= 2.5f;
-                    int dust2 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 41, projectile.velocity.X * 1.2f, projectile.velocity.Y * 1.2f, 75, default(Color), 1.5f); //this defines the flames dust and color parcticles, like when they fall thru ground, change DustID to wat dust you want from Terraria
+                    int dust2 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 41, projectile.velocity.X * 1.2f, projectile.velocity.Y * 1.2f, 75, default(Color), 1f); //this defines the flames dust and color parcticles, like when they fall thru ground, change DustID to wat dust you want from Terraria
                 }
             
     
